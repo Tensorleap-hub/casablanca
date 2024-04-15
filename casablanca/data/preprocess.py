33f8fd551f2653e4b796104e97b3e2a5cbc3a174
Chen Rothschild <chen.rothschild@tensorleap.ai>
2024-04-15 10:43:29 +0300

    change the preprocess flow

--- casablanca/data/preprocess.py
+++ casablanca/data/preprocess.py
@@ -25,28 +25,21 @@ def load_data(set):
 def count_frames(video_path):
     vid_dict = read_video(video_path, pts_unit='sec')
     vid = vid_dict[0].permute(0, 3, 1, 2)
-    if vid.shape[2] != 256:
-        vid = nn.functional.interpolate(vid, size=(256, 256), mode='bilinear', align_corners=False)
-    vid = vid.unsqueeze(0)
-    vid_norm = (vid / 255.0 - 0.5) * 2.0
-
-    return vid_norm.shape[1]
+    return vid.shape[0]
 
 
-def count_videos(directory):
+def count_videos(directory, state):
     frame_counts = {}
     for subdir in directory:
-        file_name = subdir.split('/')[-1]
         for folder in os.listdir(subdir):
             folder_path = os.path.join(subdir, folder)
-            file_name = file_name + '/' + folder
             if os.path.isdir(folder_path):
                 for file in os.listdir(folder_path):
                     if file.endswith('.mp4'):
-                        file_name = file_name + '/' + file
                         video_path = os.path.join(subdir, folder, file)
                         num_frames = count_frames(video_path)
-                        frame_counts[file_name] = num_frames
+                        frame_counts[subdir.split('/')[-1] + '/' + folder + '/' + file] = num_frames
+        save_frame_counts(frame_counts, f'{state}.json')
 
     return frame_counts
 
@@ -61,12 +54,9 @@ def extract_id(directory):
 
 
 if __name__ == '__main__':
-    state = 'test'
+    state = 'dev'
     video_directory = f'/Users/chenrothschild/Tensorleap/data/casablanca/{state}/mp4'
     dirs = [os.path.join(video_directory, d) for d in os.listdir(video_directory) if d != '.DS_Store']
     sorted_dirs = sorted(dirs, key=extract_id)
-    frame_counts = count_videos(dirs)
-    # Path to the output JSON file
-    output_json_file = f'{state}.json'
-    # Save the frame counts dictionary as a JSON file
-    save_frame_counts(frame_counts, output_json_file)
+    frame_counts = count_videos(dirs, state)
+    save_frame_counts(frame_counts, f'{state}.json')
