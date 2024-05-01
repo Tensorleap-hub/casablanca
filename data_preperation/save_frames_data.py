e0bd90ddb52814143e55e972a3200391a59e60ac
Danielle Ben Bashat <daniellebenba@gmail.com>
2024-05-01 12:10:58 +0300

    save locally frames

--- data_preperation/save_frames_data.py
+++ data_preperation/save_frames_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from tqdm import tqdm
 from functools import lru_cache
 from os import environ
 import json
@@ -22,11 +22,15 @@ if __name__ == "__main__":
     config = CONFIG['dataset_creation']
     files_df = pd.read_csv(_download(CONFIG['data_filepath']), sep='\t')
     frames_indices = [0] + list_indices()
-    for i, row in files_df.iterrows():
+    for i, row in tqdm(files_df.iterrows()):
         for frame_number in frames_indices:
             path = row['path']
             root, extension = os.path.splitext(path)
             fpath = _download(path)
+            save_path = fpath.rsplit('.', 1)[0] + CONFIG['frame_separator'] + str(frame_number) + '.png'
+            save_path = save_path.replace('mp4', 'frames')
+            if os.path.exists(save_path):
+                continue
             # if fpath.rsplit('.', 1)[-1] == 'mp4':
             try:
                 frame = input_video(fpath, frame_number)
@@ -35,8 +39,8 @@ if __name__ == "__main__":
                 continue
             frame = rescale_min_max(frame.numpy()).transpose((1, 2, 0))
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            save_path = fpath.rsplit('.', 1)[0] + CONFIG['frame_separator'] + str(frame_number) + '.png'
-            save_path = save_path.replace('mp4', 'frames')
             if not os.path.exists(os.path.dirname(save_path)):
                 os.makedirs(os.path.dirname(save_path))
             cv2.imwrite(save_path, frame)
+
+    print(f"Done")
