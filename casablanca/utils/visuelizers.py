e63d8d6f082bba5a7e47ff6f22455cfbc75f524b
Danielle Ben Bashat <daniellebenba@gmail.com>
2024-05-01 11:56:31 +0300

    add grid all vis

--- casablanca/utils/visuelizers.py
+++ casablanca/utils/visuelizers.py
@@ -13,3 +13,11 @@ def grid_frames(first_frame: np.ndarray, current_frame: np.ndarray) -> LeapImage
     # current_frame_transposed = np.transpose(current_frame, (1, 2, 0))
     concatenated_image = (np.hstack((rescale_min_max(first_frame), rescale_min_max(current_frame))))
     return LeapImage(concatenated_image)
+
+
+
+def grid_all(first_frame: np.ndarray, current_frame: np.ndarray, source_image: np.ndarray, pred_image: np.ndarray) -> LeapImage:
+    first_row = np.hstack((rescale_min_max(source_image), rescale_min_max(pred_image)))
+    sec_row = np.hstack((rescale_min_max(first_frame), rescale_min_max(current_frame)))
+    concatenated_image = np.vstack((first_row, sec_row))
+    return LeapImage(concatenated_image)
