22bc3a190a9e24f7723c64c58068875adad0037f
RanHomri <69201624+RanHomri@users.noreply.github.com>
2024-05-05 14:46:09 +0300

    formatting (#11)
    Co-authored-by: ranhomri <ran.homri@tensorleap.ai>

--- casablanca/utils/visuelizers.py
+++ casablanca/utils/visuelizers.py
@@ -3,20 +3,17 @@ from code_loader.utils import rescale_min_max
 import numpy as np
 
 
-def Image_change_last(image: np.ndarray) -> LeapImage:
-    img_np_transposed = np.transpose(image, (1, 2, 0))
-    return LeapImage(img_np_transposed.astype(np.float32))
+def image_change_last(image: np.ndarray) -> LeapImage:
+    return LeapImage(image.astype(np.float32))
 
 
 def grid_frames(first_frame: np.ndarray, current_frame: np.ndarray) -> LeapImage:
-    # first_frame_transposed = np.transpose(first_frame, (1, 2, 0))
-    # current_frame_transposed = np.transpose(current_frame, (1, 2, 0))
     concatenated_image = (np.hstack((rescale_min_max(first_frame), rescale_min_max(current_frame))))
     return LeapImage(concatenated_image)
 
 
-
-def grid_all(first_frame: np.ndarray, current_frame: np.ndarray, source_image: np.ndarray, pred_image: np.ndarray) -> LeapImage:
+def grid_all(first_frame: np.ndarray, current_frame: np.ndarray, source_image: np.ndarray,
+             pred_image: np.ndarray) -> LeapImage:
     first_row = np.hstack((rescale_min_max(source_image), rescale_min_max(pred_image)))
     sec_row = np.hstack((rescale_min_max(first_frame), rescale_min_max(current_frame)))
     concatenated_image = np.vstack((first_row, sec_row))
