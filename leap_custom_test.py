e59af5ab2152a5bbfdb9241d6d9755236dc2f519
Chen Rothschild <chen.rothschild@tensorleap.ai>
2024-05-28 14:59:34 +0300

    .

--- leap_custom_test.py
+++ leap_custom_test.py
@@ -5,10 +5,14 @@ import onnxruntime
 import numpy as np
 import tensorflow as tf
 
+from casablanca.config import CONFIG
+from casablanca.data.preprocess import load_data_all
+from casablanca.data.save_frames_to_gcs import process_frame, save_frames_to_gcs
+from casablanca.utils.gcs_utils import _connect_to_gcs_and_return_bucket
 from casablanca.utils.loss import lpip_loss_alex, lpip_loss_vgg, dummy_loss
 from casablanca.utils.metrics import lpip_alex_metric, lpip_vgg_metric, l1
-from casablanca.utils.visuelizers import image_change_last, grid_frames
-from leap_binder import input_encoder_source_image, preprocess_func, input_encoder_current_frame, \
+from casablanca.utils.visuelizers import image_change_last, grid_frames, grid_all
+from leap_binder import input_encoder_first_frame, preprocess_func, input_encoder_current_frame, \
     input_encoder_first_frame, calc_metadata_vals, calc_metadata_stats_func
 
 
@@ -27,46 +31,55 @@ def check_custom_test():
     # input_name_3 = sess.get_inputs()[2].name
     # output_names = [output.name for output in sess.get_outputs()]
 
+    # t = process_frame(0,
+    #                   '/Users/chenrothschild/tensorleap/data/casablanca-datasets/data/dev/mp4/id00012/O2jVZ--4vCk/00067.mp4',
+    #                   _connect_to_gcs_and_return_bucket(CONFIG['BUCKET_NAME']))
+
     subsets = preprocess_func()
     for set in subsets:
         for idx in range(set.length):
-            first_frame = input_encoder_first_frame(idx, set)
+            source_image = first_frame = input_encoder_first_frame(idx, set)
             current_frame = input_encoder_current_frame(idx, set)
-            source_image = input_encoder_source_image(idx, set)
 
             # pred = sess.run(output_names, {input_name_1: np.expand_dims(source_image, 0),
             #                                input_name_2: np.expand_dims(first_frame, 0),
             #                                input_name_3: np.expand_dims(current_frame, 0)})[0]
             #
-            batch_input = tf.convert_to_tensor(np.expand_dims(source_image, 0))
+
+            # metrics
+            batch_input = tf.convert_to_tensor(np.expand_dims(current_frame, 0))
             t0 = time.time()
             l1_res = l1(batch_input, batch_input)
             t1 = time.time()
             print(f'l1 time: {round(t1 - t0, 3)}')
             t0 = time.time()
-            loss_alex = lpip_alex_metric(np.expand_dims(source_image, 0), np.expand_dims(source_image, 0))
+            loss_alex = lpip_alex_metric(np.expand_dims(current_frame, 0), np.expand_dims(current_frame, 0))
             t1 = time.time()
             print(f'lpip_alex_metric time: {round(t1 - t0, 3)}')
             t0 = time.time()
-            loss_vgg = lpip_vgg_metric(np.expand_dims(source_image, 0), np.expand_dims(source_image, 0))
+            loss_vgg = lpip_vgg_metric(np.expand_dims(current_frame, 0), np.expand_dims(current_frame, 0))
             t1 = time.time()
             print(f'lpip_vgg_metric time: {round(t1 - t0, 3)}')
-            dummy_loss_ = dummy_loss(np.expand_dims(source_image, 0), np.expand_dims(source_image, 0))
+            dummy_loss_ = dummy_loss(np.expand_dims(current_frame, 0), np.expand_dims(current_frame, 0))
 
+            # vis
+            pred_image = current_frame
+            grid_all_ = grid_all(first_frame, current_frame, source_image, pred_image)
             grid_frames_ = grid_frames(first_frame, current_frame)
             source_image_vis = image_change_last(source_image)
             current_frame_vis = image_change_last(current_frame)
             first_frame_vis = image_change_last(first_frame)
-            res = calc_metadata_vals(idx, set)
-            video_path_of_source_image = res['vid_path']
-            id_of_source_image = res['source_id']
-            print(f'id_of_source_image: {id_of_source_image}')
-            print(f'video_path_of_source_image: {video_path_of_source_image}')
-            res = calc_metadata_stats_func('source_image')(idx, set)
-            res = calc_metadata_stats_func('current_frame')(idx, set)
+
+            source_res = calc_metadata_stats_func('source_image')(idx, set)
+            current_res = calc_metadata_stats_func('current_frame')(idx, set)
+            metadata_vals = calc_metadata_vals(idx, set)
 
     print("successfully!")
 
 
 if __name__ == "__main__":
-    check_custom_test()
+    data = load_data_all()
+    dev = data[data['subset'] == 'dev']
+    train_df = save_frames_to_gcs(dev)
+
+    # check_custom_test()
