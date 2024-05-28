da632aea64dd0d8d7b17bda9253fea24faca4208
Chen Rothschild <chen.rothschild@tensorleap.ai>
2024-05-28 15:00:50 +0300

    change variables names

--- casablanca/utils/metrics.py
+++ casablanca/utils/metrics.py
@@ -4,26 +4,26 @@ import lpips
 import tensorflow as tf
 
 
-def lpip_alex_metric(src_image, pred_image):
-    src_image = turn_to_pytorch_tensor(src_image)
+def lpip_alex_metric(current_frame, pred_image):
+    current_frame = turn_to_pytorch_tensor(current_frame)
     pred_image = turn_to_pytorch_tensor(pred_image)
-    src_image = src_image.permute(0, 3, 1, 2)
+    current_frame = current_frame.permute(0, 3, 1, 2)
     pred_image = pred_image.permute(0, 3, 1, 2)
     loss_fn_alex = lpips.LPIPS(net='alex')
-    result = loss_fn_alex(src_image, pred_image)
+    result = loss_fn_alex(current_frame, pred_image)
     result = result.detach()
     result = result.numpy()
     result = tf.convert_to_tensor(result)
     return result[0, 0, 0]
 
 
-def lpip_vgg_metric(src_image, pred_image):
-    src_image = turn_to_pytorch_tensor(src_image)
+def lpip_vgg_metric(current_frame, pred_image):
+    current_frame = turn_to_pytorch_tensor(current_frame)
     pred_image = turn_to_pytorch_tensor(pred_image)
-    src_image = src_image.permute(0, 3, 1, 2)
+    current_frame = current_frame.permute(0, 3, 1, 2)
     pred_image = pred_image.permute(0, 3, 1, 2)
     loss_fn_vgg = lpips.LPIPS(net='vgg')
-    result = loss_fn_vgg(src_image, pred_image)
+    result = loss_fn_vgg(current_frame, pred_image)
     result = result.detach()
     result = result.numpy()
     result = tf.convert_to_tensor(result)
